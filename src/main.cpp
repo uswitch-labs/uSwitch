596a60828ab82cdf26ccba0e7608f6ed4b745101
uswitch-labs <jackodwyer.bot@gmail.com>
2024-10-09 23:43:43 +0900

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,14 @@
 #include "./include/uswitchlabs/mainwindow.h"
-
 #include <QApplication>
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <vulkan/vulkan.h>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
+
     return a.exec();
 }
