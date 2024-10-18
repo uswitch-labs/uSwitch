fbbe4691cf9395c998690e16c8e5f445531dc0b0
uswitch-labs <jackodwyer.bot@gmail.com>
2024-10-18 18:53:04 +1100

    Create settingsdialogue.h

---
+++ include/settingsdialogue.h
@@ -0,0 +1,28 @@
+#ifndef SETTINGSDIALOG_H
+#define SETTINGSDIALOG_H
+
+#include <QDialog>
+
+namespace Ui {
+class SettingsDialog;
+}
+
+class SettingsDialog : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit SettingsDialog(QWidget *parent = nullptr);
+    ~SettingsDialog();
+
+    QString getRomDirectory() const;
+
+private slots:
+    void on_selectButton_clicked();
+
+private:
+    Ui::SettingsDialog *ui;
+    QString romDirectory;
+};
+
+#endif // SETTINGSDIALOG_H
